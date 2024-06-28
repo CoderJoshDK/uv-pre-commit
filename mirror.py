8694a35ffde50903132b0b2d395d421813939037
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2024-06-28 07:43:50 -0400

    feat: ignore yanked releases

--- mirror.py
+++ mirror.py
@@ -13,9 +13,28 @@ def main():
     with open(Path(__file__).parent / "pyproject.toml", "rb") as f:
         pyproject = tomllib.load(f)
 
-    all_versions = get_all_versions()
+    all_releases = get_all_releases()
     current_version = get_current_version(pyproject=pyproject)
-    target_versions = [v for v in all_versions if v > current_version]
+    all_non_yanked_versions = sorted([
+        release_version
+        for release_version, release in all_releases.items()
+        if not any([asset["yanked"] for asset in release])
+    ])
+
+    target_versions = [v for v in all_non_yanked_versions if v > current_version]
+
+    if not target_versions and any([
+        asset["yanked"] for asset in all_releases[current_version]
+    ]):
+        version = all_non_yanked_versions[-1]
+        paths = process_version(version)
+        if subprocess.check_output(["git", "status", "-s"]).strip():
+            subprocess.run(["git", "add", *paths], check=True)
+            subprocess.run(
+                ["git", "commit", "-m", f"Mirror: yanked {version}"], check=True
+            )
+            subprocess.run(["git", "tag", "--delete", f"{version}"], check=True)
+        return
 
     for version in target_versions:
         paths = process_version(version)
@@ -27,13 +46,15 @@ def main():
             print(f"No change {version}")
 
 
-def get_all_versions() -> list[Version]:
+def get_all_releases() -> dict[Version, list[dict[str, typing.Any]]]:
     response = urllib3.request("GET", "https://pypi.org/pypi/uv/json")
     if response.status != 200:
         raise RuntimeError("Failed to fetch versions from pypi")
 
-    versions = [Version(release) for release in response.json()["releases"]]
-    return sorted(versions)
+    return {
+        Version(release_version): release
+        for release_version, release in response.json()["releases"].items()
+    }
 
 
 def get_current_version(pyproject: dict) -> Version:
